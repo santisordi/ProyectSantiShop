8136f2110d38c7fc7e5b9bcf922d69893dedd462
Santiago <santiagosordi@gmail.com>
2023-06-06 16:40:50 -0300

    inicio repo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <a href='#' className='logo'><h1>SantiShop</h1></a>
+        <ul className='menu'>
+            <li><a className='menu-link' href='#'>Inicio</a></li>
+            <li><a className='menu-link' href='#'>Nosotros</a></li>
+            <li><a className='menu-link' href='#'>Productos</a></li>
+            <li><a className='menu-link' href='#'>Contacto</a></li>
+
+        </ul>
+        
+    </nav>
+    
+  )
+}
+
+export default Navbar;
